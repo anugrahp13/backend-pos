3611a729010748e3dde6a2cd14c2b9d38db299e8
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-21 13:31:07 +0700

    refactor[ProductController.js]: add function updateProduct from controller

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -4,6 +4,9 @@ const express = require("express");
 // Import prisma client untuk berinteraksi dengan database
 const prisma = require("../prisma/client");
 
+// Import fs untuk operasi sistem file seperti menghapus file
+const fs = require('fs');
+
 // Fungsi findProducts untuk mengambil daftar produk dengan paginasi
 const findProducts = async (req, res) => {
     try {
@@ -205,6 +208,79 @@ const findProductById = async (req, res) => {
     }
 };
 
+// Fungsi updateProduct untuk memperbarui produk yang ada
+const updateProduct = async (req, res) => {
+    // Mengambil ID dari parameter
+    const { id } = req.params;
+
+    try {
+        // Mengupdate produk dengan atau tanpa gambar
+        const dataProduct = {
+            barcode: req.body.barcode,
+            title: req.body.title,
+            description: req.body.description,
+            buy_price: parseInt(req.body.buy_price),
+            sell_price: parseInt(req.body.sell_price),
+            stock: parseInt(req.body.stock),
+            category_id: parseInt(req.body.category_id),
+            updated_at: new Date(),
+        };
+
+        // cek apakah ada gambar yang diupload
+        if (req.file) {
+
+            // Mengassign gambar ke object data produk
+            dataProduct.image = req.file.path;
+
+            // get product by id
+            const product = await prisma.product.findUnique({
+                where: {
+                    id: Number(id),
+                },
+            });
+            if (product.image) {
+
+                // Menghapus gambar lama
+                fs.unlinkSync(product.image);
+            }
+        }
+
+        // Mengupdate produk
+        const product = await prisma.product.update({
+            where: {
+                id: Number(id),
+            },
+            data: dataProduct,
+            include: {
+                category: true,
+            }
+        });
+
+        // Mengirim respons
+        res.status(200).send({
+            //meta untuk respons JSON
+            meta: {
+                success: true,
+                message: "Produk berhasil diperbarui",
+            },
+            //data produk
+            data: product,
+        });
+
+    } catch (error) {
+        // Mengirim respons jika terjadi kesalahan
+        res.status(500).send({
+            //meta untuk respons JSON
+            meta: {
+                success: false,
+                message: "Kesalahan internal server",
+            },
+            //data kesalahan
+            errors: error,
+        });
+    }
+};
+
 
 // Mengekspor fungsi-fungsi untuk digunakan di file lain
-module.exports = { findProducts, createProduct, findProductById };
+module.exports = { findProducts, createProduct, findProductById, updateProduct };
