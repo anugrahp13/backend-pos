cf36ec6e2ebefad8e3bd7e72bc68c997f8d91bdd
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-21 10:22:01 +0700

    refactor[index.js]: Create route API create data category

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,86 @@
 // Import express
-const express = require('express');
+const express = require("express");
 
 // Init express router
 const router = express.Router();
 
 // Import validators and middleware
-const { validateLogin, validateUser } = require('../utils/validators');
-const { handleValidationErrors, verifyToken } = require('../middlewares');
+const {
+  validateLogin,
+  validateUser,
+  validateCategory,
+} = require("../utils/validators");
+const {
+  handleValidationErrors,
+  verifyToken,
+  upload,
+} = require("../middlewares");
 
 // Import controllers
-const loginController = require('../controllers/LoginController');
-const userController = require('../controllers/UserController');
-const categoryController = require('../controllers/CategoryController');
+const loginController = require("../controllers/LoginController");
+const userController = require("../controllers/UserController");
+const categoryController = require("../controllers/CategoryController");
 
 // Define routes
 const routes = [
   // Login route
-  { method: 'post', path: '/login', middlewares: [validateLogin, handleValidationErrors], handler: loginController.login },
+  {
+    method: "post",
+    path: "/login",
+    middlewares: [validateLogin, handleValidationErrors],
+    handler: loginController.login,
+  },
 
   // User routes
-  { method: 'get', path: '/users', middlewares: [verifyToken], handler: userController.findUsers },
-  { method: 'post', path: '/users', middlewares: [verifyToken, validateUser, handleValidationErrors], handler: userController.createUser },
-  { method: 'get', path: '/users/:id', middlewares: [verifyToken], handler: userController.findUserById },
-  { method: 'put', path: '/users/:id', middlewares: [verifyToken, validateUser, handleValidationErrors], handler: userController.updateUser },
-  { method: 'delete', path: '/users/:id', middlewares: [verifyToken], handler: userController.deleteUser },
+  {
+    method: "get",
+    path: "/users",
+    middlewares: [verifyToken],
+    handler: userController.findUsers,
+  },
+  {
+    method: "post",
+    path: "/users",
+    middlewares: [verifyToken, validateUser, handleValidationErrors],
+    handler: userController.createUser,
+  },
+  {
+    method: "get",
+    path: "/users/:id",
+    middlewares: [verifyToken],
+    handler: userController.findUserById,
+  },
+  {
+    method: "put",
+    path: "/users/:id",
+    middlewares: [verifyToken, validateUser, handleValidationErrors],
+    handler: userController.updateUser,
+  },
+  {
+    method: "delete",
+    path: "/users/:id",
+    middlewares: [verifyToken],
+    handler: userController.deleteUser,
+  },
 
   // Category routes
-  { method: 'get', path: '/categories', middlewares: [verifyToken], handler: categoryController.findCategories },
+  {
+    method: "get",
+    path: "/categories",
+    middlewares: [verifyToken],
+    handler: categoryController.findCategories,
+  },
+  {
+    method: "post",
+    path: "/categories",
+    middlewares: [
+      verifyToken,
+      upload.single("image"),
+      validateCategory,
+      handleValidationErrors,
+    ],
+    handler: categoryController.createCategory,
+  },
 ];
 
 // Helper function to create routes
